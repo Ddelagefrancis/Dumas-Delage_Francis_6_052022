3dd242720b84e7a5ed54259e71824bcb040c34b9
FrancisDumas-Delage_3_25012022 <dumasdelagefrancis@gmail.com>
2022-05-24 16:07:37 +0200

    Correction Suppression d'une sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -30,16 +30,16 @@ exports.modifySauce = (req, res, next) => {
 
 // Suppression d'une sauce 
 exports.deleteSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => {
-            const filename = sauce.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-                Sauce.deleteOne({ _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Sauce supprimé !' }))
-                    .catch(error => res.status(400).json({ error }));
-            })
-        })
-        .catch(error => res.status(500).json({ error }));
+    // Sauce.findOne({ _id: req.params.id })
+    //     .then(sauce => {
+    //         const filename = sauce.imageUrl.split('/images/')[1];
+    //         fs.unlink(`images/${filename}`, () => {
+    //             Sauce.deleteOne({ _id: req.params.id })
+    //                 .then(() => res.status(200).json({ message: 'Sauce supprimé !' }))
+    //                 .catch(error => res.status(400).json({ error }));
+    //         })
+    //     })
+    //     .catch(error => res.status(500).json({ error }));
     Sauce.findOne({ _id: req.params.id })
         .then((Sauce) => {
             if (!Sauce) {

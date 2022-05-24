0ab12d5e43cdcf56d246e43dbde4a8db9fd3734b
FrancisDumas-Delage_3_25012022 <dumasdelagefrancis@gmail.com>
2022-05-24 16:07:07 +0200

    correction [ ] à la place de ( ).

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
     // Nom de fichier à utiliser par multer 
     filename: (req, file, callback) => {
         const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPES(file.mimetype);
+        const extension = MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + '.' + extension);
     }
 });

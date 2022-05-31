e072170e1e6d428ae13385fc3b1d78eb0933e5b3
FrancisDumas-Delage_3_25012022 <dumasdelagefrancis@gmail.com>
2022-05-31 16:43:47 +0200

    Ajout E-mail Validator

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 // importation middleware / password
 const password = require("../middleware/password");
-
+const email = require("../middleware/email");
 const connexion = require('../middleware/limit-connexion');
 
 // importation controllers / user
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', password, userCtrl.signup);
+router.post('/signup', password, email, userCtrl.signup);
 router.post('/login', connexion, userCtrl.login);
 
 

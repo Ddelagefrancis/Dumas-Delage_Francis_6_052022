dd4ed9db3c61578cc135b5090c36315861a580ae
FrancisDumas-Delage_3_25012022 <dumasdelagefrancis@gmail.com>
2022-06-02 22:10:20 +0200

    Correction suppression image à la modification

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -16,14 +16,24 @@ exports.createSauce = (req, res, next) => {
 
 // Modification d'une sauce 
 exports.modifySauce = (req, res, next) => {
-    const sauceObject = req.file ?
-        {
-            ...JSON.parse(req.body.sauce),
-            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`} : { ...req.body };
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            const sauceObject = req.file ?
+                {
+                    ...JSON.parse(req.body.sauce),
+                    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+                } : { ...req.body };
 
-    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Sauce modifié !' }))
-        .catch(error => res.status(400).json({ error }));
+            fs.unlink(`images/${filename}`, () => {
+                Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Sauce modifié !' }))
+                    .catch(error => res.status(400).json({ error }));
+
+            })
+
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 // Suppression d'une sauce 
